2c06430a1795282d8c465ac3ecba084131614639
laihanbin <512412344@qq.com>
2023-07-04 15:46:54 +0800

    feat: technology panel

--- src/modules/renderer/TechnologyPanel.ts
+++ src/modules/renderer/TechnologyPanel.ts
@@ -1,3 +1,62 @@
+import type { ImageResource } from '../../types'
+import { loadImages } from '../../utils/tools'
 import { Renderer } from '../base'
+import type { GameObject } from '../gameObject'
 
-export default class TechnologyPanelRenderer extends Renderer {}
+export default class TechnologyPanelRenderer extends Renderer {
+  data: ImageResource[] = []
+
+  /**
+   * @todo 这里的资源加载后期都应统一全局加载
+   */
+  async init () {
+    this.data = await loadImages([
+      {
+        name: 'border1',
+        src: '/border1.png',
+        width: 48,
+        height: 48
+      },
+      {
+        name: 'border2',
+        src: '/border2.png',
+        width: 48,
+        height: 48
+      }
+    ])
+  }
+
+  draw (gameObjects: Map<string, GameObject>) {
+    this.clear()
+
+    gameObjects.forEach(gameObject => {
+      this.drawAvatar(gameObject)
+    })
+  }
+
+  drawAvatar (gameObject: GameObject) {
+    const {
+      ctx
+    } = this
+
+    const {
+      shape: {
+        midpoint: { x, y },
+        width,
+        height
+      }
+    } = gameObject
+
+    /** @todo */
+    const border = this.data.find(item => item.name === 'border1')!
+
+    ctx.save()
+    ctx.translate(x, y)
+
+    ctx.drawImage(border.img, -border.width / 2, -border.height / 2, border.width, border.height)
+
+    ctx.drawImage(gameObject.shape.model!.img, -width / 4, -height / 4, width / 2, height / 2)
+
+    ctx.restore()
+  }
+}
