4efcd6c65666d59e9924266bdcbd68527e829585
Kkk <2568143457@qq.com>
2023-07-05 17:02:06 +0800

    chore: 添加子弹生成方法

--- src/modules/gameObject/Skill.ts
+++ src/modules/gameObject/Skill.ts
@@ -1,5 +1,9 @@
-import { GameObjectTypes } from '../../config'
+import { ActionTypes, GameObjectTypes } from '../../config'
 import { createRandomId } from '../../utils/tools'
+import { IntervalAction } from '../action'
+import { Trigger } from '../trigger'
+// import { BulletGameObject } from './Bullet'
+import { type EnemyGameObject } from './Enemy'
 import { GameObject } from './GameObject'
 
 type Level = Record<string, Partial<SkillGameObject>>
@@ -50,6 +54,7 @@ export interface SkillGameObject {
 }
 
 export class SkillGameObject extends GameObject {
+  triggers = new Set<Trigger>()
   constructor ({
     id,
     type
@@ -58,7 +63,7 @@ export class SkillGameObject extends GameObject {
       type: GameObjectTypes.SKILL
     })
     for (const [key, value] of Object.entries(LevelNumeric.Level1)) {
-      // @ts-expect-error no-wrong-here
+      // @ts-expect-error stable-type-here
       this[key] = value
     }
     type === 'Tower' ? this.initTower() : this.initEnemy()
@@ -73,7 +78,23 @@ export class SkillGameObject extends GameObject {
     //
   }
 
-  generateBullet () {
-    //
+  generateBullet (enemy: EnemyGameObject) {
+    this.triggers.add(new Trigger({
+      conditions: [
+        (_gameObject, trigger) => trigger.isTimeout(1000),
+        () => !enemy.shape.isEntered(this.shape)
+      ],
+      actions: [
+        new IntervalAction<ActionTypes.CREATE>({
+          interval: 1000,
+          type: ActionTypes.CREATE,
+          callback () {
+            // const bullet = new BulletGameObject()
+
+            // console.log(bullet)
+          }
+        })
+      ]
+    }))
   }
 }
